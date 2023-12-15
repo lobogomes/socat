70b9937976872ad760410d6ca310fe9184931034
Amanda Lobo <8lobogomes@gmail.com>
2023-12-15 11:56:04 -0300

    1st commit

---
+++ src/test/java/com/socat/so/SocatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socat.so;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
